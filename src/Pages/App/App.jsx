9257c248f88f37fc16633f5d1ef71c281791ea3a
nicolas <nicolasgpla@gmail.com>
2024-07-18 21:02:59 -0500

    build: initial configuration, installation of dependencies

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl text-indigo-700 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
